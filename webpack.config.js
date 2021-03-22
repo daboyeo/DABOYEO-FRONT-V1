be726e1b87b374020c17e18709008d0097ea384a
YeongG <yeonggil223@gmail.com>
2021-03-22 11:05:16 +0900

    [CREATE] webpack setting

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "none",
+  entry: {
+    app: path.join(__dirname, "src", "index.tsx"),
+  },
+  target: "web",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.(jpg|png|jpeg|bmp|gif|svg)?$/,
+        loader: "file-loader",
+      },
+    ],
+  },
+  output: {
+    filename: "bundle.min.js",
+    path: path.resolve(__dirname, "s3"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+};
