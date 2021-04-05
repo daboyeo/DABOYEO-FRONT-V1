3e1bbe43288c27ded252721ee5958226065c18f7
YeongG <yeonggil223@gmail.com>
2021-04-05 23:47:26 +0900

    [CREATE] asset index.ts

---
+++ src/Asset/index.ts
@@ -0,0 +1,5 @@
+export { default as HomeIcon } from "./HomeIcon.svg";
+export { default as PersonIcon } from "./PersonIcon.svg";
+export { default as SearchIcon } from "./SearchIcon.svg";
+export { default as CheckIcon } from "./CheckIcon.svg";
+export { default as RejectIcon } from "./RejectIcon.svg";
