7c1a6c9eaae5272039975c80e73c22f1522816e1
YeongG <yeonggil223@gmail.com>
2021-04-13 23:46:18 +0900

    [MODIFY] component index.ts

---
+++ src/Component/index.ts
@@ -0,0 +1,5 @@
+export { default as WriteFeed } from "./WriteFeed/WriteFeed";
+export { default as Header } from "./Header/Header";
+export { default as FeedItem } from "./FeedItem/FeedItem";
+export { default as OverViewImg } from "./OverViewImg/OverViewImg";
+export { default as CommentItem } from "./CommentItem/CommentItem";
