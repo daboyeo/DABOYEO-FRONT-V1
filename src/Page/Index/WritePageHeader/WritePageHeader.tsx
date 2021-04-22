217661566e240ae7ef8b9f63ecf2e16e016646d9
YeongG <yeonggil223@gmail.com>
2021-04-22 22:45:09 +0900

    [MODIFY] WriteFeed Props interface 변경에 따른 코드 변경

--- src/Page/Index/WritePageHeader/WritePageHeader.tsx
+++ src/Page/Index/WritePageHeader/WritePageHeader.tsx
@@ -1,15 +1,18 @@
 import React, { FC, useCallback } from "react";
 import { useDispatch } from "react-redux";
 import { WriteFeed } from "../../../Component";
+import { setWriteModalOpen } from "../../../Module/Action/writeModal";
 
 const WritePageHeader: FC = () => {
   const disaptch = useDispatch();
-  const clickHandler = useCallback(() => {}, []);
+  const clickHandler = useCallback(() => {
+    disaptch(setWriteModalOpen(true));
+  }, []);
   return (
     <WriteFeed
       onClick={clickHandler}
       imgSrc="https://image.flaticon.com/icons/png/512/634/634741.png"
-      placeHolder="공영길님, 어떤 사건 사고를 목격하셨나요?"
+      btnText="공영길님, 어떤 사건 사고를 목격하셨나요?"
     />
   );
 };
