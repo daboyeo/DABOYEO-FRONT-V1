cdaaecd42a5c6ad84ca3f7dd788e56bfc0f24651
YeongG <yeonggil223@gmail.com>
2021-04-22 22:45:30 +0900

    [MODIFY] redux index.ts

--- src/Module/Reducer/index.ts
+++ src/Module/Reducer/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import overViewImgReducer from "./overViewImg";
+import writeModalReducer from "./writeModal";
 
 const rootReducer = combineReducers({
   overViewImg: overViewImgReducer,
+  writeModal: writeModalReducer,
 });
 
 export default rootReducer;
