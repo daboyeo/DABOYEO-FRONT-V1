cbacfefea4571a1ad2e8364cad4012f69276d0c6
YeongG <yeonggil223@gmail.com>
2021-04-22 22:41:18 +0900

    [MODIFY] Component index.ts

--- src/Component/index.ts
+++ src/Component/index.ts
@@ -1,4 +1,5 @@
 export { default as WriteFeed } from "./WriteFeed/WriteFeed";
+export { default as WriteModal } from "./WriteModal/WriteModal";
 export { default as Header } from "./Header/Header";
 export { default as FeedItem } from "./FeedItem/FeedItem";
 export { default as OverViewImg } from "./OverViewImg/OverViewImg";
