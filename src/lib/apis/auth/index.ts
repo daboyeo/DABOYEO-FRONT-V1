7de186b84c35c98b83843dca1919ad83affa643b
YeongG <yeonggil223@gmail.com>
2021-04-26 23:11:31 +0900

    [CREATE] lib/api/auth 인증 관련 api

---
+++ src/lib/apis/auth/index.ts
@@ -0,0 +1,8 @@
+import apiDefault, { ResServer } from "../../client";
+
+export const reqAuth = (token: string): ResServer<{ access_token: string }> => {
+  return apiDefault().post<{ access_token: string }>("/auth", {
+    token,
+    auth_type: "google",
+  });
+};
