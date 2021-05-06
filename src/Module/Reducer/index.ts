4c657fcc24c6d673f939d713f6863a5a9a5b075c
YeongG <yeonggil223@gmail.com>
2021-05-06 23:45:50 +0900

    [MODIFY] redux reducer index.ts

--- src/Module/Reducer/index.ts
+++ src/Module/Reducer/index.ts
@@ -1,10 +1,18 @@
 import { combineReducers } from "redux";
+import loadingReducer from "./loading";
 import overViewImgReducer from "./overViewImg";
+import reportReducer from "./report";
 import writeModalReducer from "./writeModal";
+import detailFeedReducer from "./detailFeed";
+import profileReducer from "./profile";
 
 const rootReducer = combineReducers({
   overViewImg: overViewImgReducer,
   writeModal: writeModalReducer,
+  report: reportReducer,
+  loading: loadingReducer,
+  detailFeed: detailFeedReducer,
+  profile: profileReducer,
 });
 
 export default rootReducer;
