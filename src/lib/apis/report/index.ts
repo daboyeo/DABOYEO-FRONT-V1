8e64fab8fa2f5c9edff7a4e4e46ea627572f6007
YeongG <yeonggil223@gmail.com>
2021-05-06 23:46:34 +0900

    [MODIFY] lib/apis report 글쓰기, 이미지 업로드

--- src/lib/apis/report/index.ts
+++ src/lib/apis/report/index.ts
@@ -1,6 +1,20 @@
+import axios from "axios";
 import apiDefault, { ResServer } from "../../client";
-import { ReportListItem } from "../../payloads/report";
+import { ReportListItem, ReportReq } from "../../payloads/report";
 
 export const reqGetRepots = (): ResServer<{ reports: ReportListItem[] }> => {
-  return apiDefault().get<{ reports: ReportListItem[] }>(`report`);
+  return apiDefault().get<{ reports: ReportListItem[] }>(`/report`);
+};
+
+export const reqPostReport = (data: ReportReq): ResServer<{}> => {
+  return apiDefault().post<{}>("/report", data);
+};
+
+export const reqUploadImage = (
+  file: File
+): ResServer<{ file_uuid: string }> => {
+  const formData: FormData = new FormData();
+  formData.append("file", file);
+
+  return axios.post<{ file_uuid: string }>("http://54.180.105.66:8000", file);
 };
