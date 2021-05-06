4c61f8186395550c095bc28a531554f5c53f21fa
YeongG <yeonggil223@gmail.com>
2021-05-06 23:49:53 +0900

    [MODIFY] WritePageHeader 기능 구현

--- src/Page/Index/WritePageHeader/WritePageHeader.tsx
+++ src/Page/Index/WritePageHeader/WritePageHeader.tsx
@@ -1,18 +1,21 @@
 import React, { FC, useCallback } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { WriteFeed } from "../../../Component";
 import { setWriteModalOpen } from "../../../Module/Action/writeModal";
+import { Store } from "../../../Module/Reducer";
 
 const WritePageHeader: FC = () => {
   const disaptch = useDispatch();
+  const { name, profileUri } = useSelector((store: Store) => store.profile);
+
   const clickHandler = useCallback(() => {
     disaptch(setWriteModalOpen(true));
   }, []);
   return (
     <WriteFeed
       onClick={clickHandler}
-      imgSrc="https://image.flaticon.com/icons/png/512/634/634741.png"
-      btnText="공영길님, 어떤 사건 사고를 목격하셨나요?"
+      imgSrc={profileUri}
+      btnText={`${name}님, 어떤 사건 사고를 목격하셨나요?`}
     />
   );
 };
--- src/Page/WriteModal/WriteModalPage.tsx
+++ src/Page/WriteModal/WriteModalPage.tsx
@@ -1,24 +1,48 @@
 import React, { FC, useCallback, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { WriteModal } from "../../Component";
+import * as reportApi from "../../lib/apis/report";
+import { ReportManagement, ReportReq } from "../../lib/payloads/report";
 import { setWriteModalOpen } from "../../Module/Action/writeModal";
 import { Store } from "../../Module/Reducer";
 
 const WriteModalPage: FC = () => {
   const dispatch = useDispatch();
   const open: boolean = useSelector((store: Store) => store.writeModal.open);
+  const { name, profileUri } = useSelector((store: Store) => store.profile);
 
   const closeModal = useCallback(() => {
     dispatch(setWriteModalOpen(false));
   }, []);
 
+  const submit = useCallback(async (data: ReportManagement) => {
+    try {
+      const uuids: string[] = await Promise.all(
+        data.files.map(async (file: File) => {
+          const uuid: string = (await reportApi.reqUploadImage(file)).data
+            .file_uuid;
+
+          return uuid;
+        })
+      );
+
+      await reportApi.reqPostReport({
+        content: data.content,
+        image_uris: uuids,
+        location: data.location,
+        tags: data.tags,
+      });
+      alert("글 작성 성공!");
+    } catch (err) {}
+  }, []);
+
   return (
     open && (
       <WriteModal
-        imgSrc=""
-        name="공영길"
+        imgSrc={profileUri}
+        name={name}
         onBackgroundClick={closeModal}
-        onSubmit={function () {}}
+        onSubmit={submit}
         placeHolder="어떤 사건 사고를 목격하셨나요?"
         title="글 쓰기"
       />
