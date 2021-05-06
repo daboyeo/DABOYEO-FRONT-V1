429088d0513f73d887c30756e94186f9810097c3
YeongG <yeonggil223@gmail.com>
2021-05-06 23:48:26 +0900

    [MODIFY] lib client.ts 토큰 관련 상수 추가

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,12 +2,17 @@ import axios, { AxiosInstance, AxiosResponse } from "axios";
 
 export type ResServer<T> = Promise<AxiosResponse<T>>;
 
-const BASE_URL: string = "";
-const ACCESS_TOKEN: string = "ACCESS_TOKEN";
+const BASE_URL: string =
+  "https://6l8nidp3k7.execute-api.ap-northeast-2.amazonaws.com/dev";
+const ACCESS_TOKEN = "ACCESS_TOKEN" as const;
+const PROFILE = "PROFILE" as const;
+const USER_NAME = "USER_NAME" as const;
 
 export const authConstant = {
   BASE_URL,
   ACCESS_TOKEN,
+  PROFILE,
+  USER_NAME,
 };
 
 const apiDefault = (): AxiosInstance => {
