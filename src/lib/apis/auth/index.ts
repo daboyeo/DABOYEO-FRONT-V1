ad2ec46b1f328500599fb09c0b491b41837ed4dd
YeongG <yeonggil223@gmail.com>
2021-05-06 23:46:05 +0900

    [MODIFY] lib/apis auth

--- src/lib/apis/auth/index.ts
+++ src/lib/apis/auth/index.ts
@@ -1,7 +1,7 @@
 import apiDefault, { ResServer } from "../../client";
 
 export const reqAuth = (token: string): ResServer<{ access_token: string }> => {
-  return apiDefault().post<{ access_token: string }>("/auth", {
+  return apiDefault().post<{ access_token: string }>("/user/auth", {
     token,
     auth_type: "google",
   });
