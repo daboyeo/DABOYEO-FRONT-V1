cbaabcdfef9c92c219e8ecd202032b2ca2f7f256
YeongG <yeonggil223@gmail.com>
2021-05-06 23:49:39 +0900

    [MODIFY] FeedList 정적 => props

--- src/Page/Index/style.ts
+++ src/Page/Index/style.ts
@@ -6,12 +6,8 @@ export const Container = styled(GlobalLayoutDiv)`
 `;
 export const DetailFeed = styled.div`
   position: fixed;
-  z-index: 10;
   bottom: 20px;
   right: 20px;
-
-  @media (max-width: 1600px) {
-  }
 `;
 export const FeedWrap = styled.div`
   margin-top: 20px;
