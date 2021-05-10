7de8b2744329ea46e581e17b4299b71108f88b6c
YeongG <yeonggil223@gmail.com>
2021-05-10 23:55:30 +0900

    [MODIFY] DetailFeedPage redux연결

--- src/Page/Index/DetailFeed/DetailFeed.tsx
+++ src/Page/Index/DetailFeed/DetailFeed.tsx
@@ -1,15 +1,17 @@
 import React, { FC } from "react";
 import { shallowEqual, useSelector } from "react-redux";
-import { CommentItem, WriteFeed } from "../../../Component";
+import { DetailFeed } from "../../../Component";
 import { ReportListItem } from "../../../lib/payloads/report";
+import { getAfterTime, getImgSrc } from "../../../lib/utils";
 import { Store } from "../../../Module/Reducer";
 import * as S from "./style";
 
 const DetailFeedPage: FC = () => {
-  const { id, feeds } = useSelector(
+  const { id, feeds, profile } = useSelector(
     (store: Store) => ({
       id: store.detailFeed.id,
       feeds: store.report.reports,
+      profile: store.profile.profileUri,
     }),
     shallowEqual
   );
@@ -18,42 +20,15 @@ const DetailFeedPage: FC = () => {
 
   return feedData ? (
     <S.Container>
-      <S.ContentWrap>
-        <S.DetailFeedHeader>
-          <S.FeedWriterImg src={feedData.reporter_profile_uri} />
-          <S.DetailFeedInfoWrap>
-            <S.DetailFeedInfo>
-              <div>{feedData.reporter_name}</div>
-              <div>{feedData.location}</div>
-            </S.DetailFeedInfo>
-            <S.DetailFeedTime>{feedData.created_at}</S.DetailFeedTime>
-          </S.DetailFeedInfoWrap>
-        </S.DetailFeedHeader>
-        <S.DetailFeedContent>{feedData.content}</S.DetailFeedContent>
-        <S.DetailFeedImg>
-          {feedData.image_uris.map((src: string, i: number) => (
-            <S.DetailFeedImgItem key={i}>
-              <img src={src} />
-            </S.DetailFeedImgItem>
-          ))}
-        </S.DetailFeedImg>
-      </S.ContentWrap>
-      <S.CommentWrap>
-        <S.Comments>
-          <CommentItem />
-          <CommentItem />
-          <CommentItem />
-          <CommentItem />
-          <CommentItem />
-          <CommentItem />
-        </S.Comments>
-        <S.WriteComment>
-          <WriteFeed
-            imgSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqFZ0FGTrytcVajqgDEGFJX8J8kO-1WVG_wQ&usqp=CAU"
-            btnText="댓글을 입력하세요..."
-          />
-        </S.WriteComment>
-      </S.CommentWrap>
+      <DetailFeed
+        tags={feedData.tags}
+        content={feedData.content}
+        imgs={feedData.image_uris.map((uri: string) => getImgSrc(uri))}
+        location={feedData.location}
+        profileImgSrc={getImgSrc(feedData.reporter_profile_uri)}
+        reporterName={feedData.reporter_name}
+        time={getAfterTime(feedData.created_at)}
+      />
     </S.Container>
   ) : (
     <></>
