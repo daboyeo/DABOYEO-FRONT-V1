fae57de3e11cbaae33bf3f7b45e23f16057e5a8f
YeongG <yeonggil223@gmail.com>
2021-05-12 23:21:18 +0900

    [MODIFY] redux saga index.ts

--- src/Module/Saga/index.ts
+++ src/Module/Saga/index.ts
@@ -1,8 +1,9 @@
 import { all } from "@redux-saga/core/effects";
+import detailFeedSaga from "./detailFeed";
 import reportSaga from "./report";
 
 function* rootSaga() {
-  yield all([reportSaga()]);
+  yield all([reportSaga(), detailFeedSaga()]);
 }
 
 export default rootSaga;
