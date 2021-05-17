7d3c5df6e71e15ab8a171b64d4d8b2d7a8501670
YeongG <yeonggil223@gmail.com>
2021-05-17 23:42:53 +0900

    [MODIFY] redix detailFeed 최적화 제거

--- src/Module/Saga/detailFeed/index.ts
+++ src/Module/Saga/detailFeed/index.ts
@@ -8,13 +8,8 @@ import { finishLoading, startLoading } from "../../Action/loading";
 import * as reportApi from "../../../lib/apis/report";
 import { AxiosResponse } from "axios";
 import { ReportDetail } from "../../../lib/payloads/report";
-import { Store } from "../../Reducer";
 
 function* getDetailFeedSaga(action: ReturnType<typeof getDetailFeed>) {
-  const feedData: ReportDetail = yield select(
-    (store: Store) => store.detailFeed
-  );
-  if (feedData && feedData.report_id === action.payload.id) return;
   yield put(startLoading(GET_DETAIL_FEED));
   try {
     const res: AxiosResponse<ReportDetail> = yield call(
