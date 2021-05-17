d606921b5cd396b30ba2679eaa6ba4527b2927c9
YeongG <yeonggil223@gmail.com>
2021-05-17 23:19:27 +0900

    [MODIFY] /search 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { FC } from "react";
 import { Link, BrowserRouter, Switch, Route } from "react-router-dom";
 import { Header } from "./Component";
 import { GlobalStyle } from "./GlobalStyle";
-import { ClubPage, IndexPage, ProfilePage } from "./Page";
+import { ClubPage, IndexPage, ProfilePage, SearchPage } from "./Page";
 
 const App: FC = () => {
   return (
@@ -13,6 +13,7 @@ const App: FC = () => {
         <Route exact path="/" component={IndexPage} />
         <Route exact path="/profile" component={ProfilePage} />
         <Route exact path="/club" component={ClubPage} />
+        <Route exact path="/search/:keyword" component={SearchPage} />
       </Switch>
     </BrowserRouter>
   );
