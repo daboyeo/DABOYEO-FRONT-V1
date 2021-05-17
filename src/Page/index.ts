9dff294ede00638c30d53fcafeb68418f312029a
YeongG <yeonggil223@gmail.com>
2021-05-17 23:46:49 +0900

    [MODIFY] page index.ts

--- src/Page/index.ts
+++ src/Page/index.ts
@@ -1,3 +1,4 @@
 export { default as IndexPage } from "./Index/IndexPage";
 export { default as ProfilePage } from "./Profile/ProfilePage";
 export { default as ClubPage } from "./Club/ClubPage";
+export { default as SearchPage } from "./Search/SearchPage";
