109d05ad6a0ae133f63f16a6792d487ecabfc5d5
YeongG <yeonggil223@gmail.com>
2021-05-23 23:28:32 +0900

    [MODIFY] /profile => /profile/:id 라우팅 변경

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import { Link, BrowserRouter, Switch, Route } from "react-router-dom";
 import { Header } from "./Component";
 import { GlobalStyle } from "./GlobalStyle";
 import { ClubPage, IndexPage, ProfilePage, SearchPage } from "./Page";
+import DetailFeedPage from "./Page/Index/DetailFeed/DetailFeed";
 
 const App: FC = () => {
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Header />
+      <DetailFeedPage />
       <Switch>
         <Route exact path="/" component={IndexPage} />
-        <Route exact path="/profile" component={ProfilePage} />
+        <Route exact path="/profile/:id" component={ProfilePage} />
         <Route exact path="/club" component={ClubPage} />
         <Route exact path="/search/:keyword" component={SearchPage} />
       </Switch>
