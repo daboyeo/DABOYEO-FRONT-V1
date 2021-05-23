57df5d8487fb3bcd94d9e4a464a517b70bba6fe3
YeongG <yeonggil223@gmail.com>
2021-05-23 23:41:43 +0900

    [MODIFY] lib/client.ts authConstant 추가

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,17 +2,20 @@ import axios, { AxiosInstance, AxiosResponse } from "axios";
 
 export type ResServer<T> = Promise<AxiosResponse<T>>;
 
-export const BASE_URL = "https://6l8nidp3k7.execute-api.ap-northeast-2.amazonaws.com/dev" as const;
+export const BASE_URL =
+  "https://6l8nidp3k7.execute-api.ap-northeast-2.amazonaws.com/dev" as const;
 export const FILE_SERVER_BASE_URL = "http://54.180.105.66:8000" as const;
 
 const ACCESS_TOKEN = "ACCESS_TOKEN" as const;
 const PROFILE = "PROFILE" as const;
 const USER_NAME = "USER_NAME" as const;
+const USER_ID = "USER_ID" as const;
 
 export const authConstant = {
   ACCESS_TOKEN,
   PROFILE,
   USER_NAME,
+  USER_ID,
 };
 
 const apiDefault = (): AxiosInstance => {
